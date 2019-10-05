b40a763566370f48e2b0a1a4be9718e476fbac4e
sjdsh <samljdixon@gmail.com>
2019-10-05 12:37:17 -0400

    Adds functions for getting global note path and asset path Changes global_note_path var to constant all caps adds asset var moves route creation to seperate function

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ import os
 import sys
 from bottle import request, route, run, template, static_file
 import mistune
-import routes
 
-# Hacky code right now
-# Tries to take a path to notes
-# If you don't give it a path it works relatively current working dir 
-# Else it looks to path you give it for notes
-try:
-    if os.path.exists(sys.argv[1]):
-        global_path = sys.argv[1]
-except:
-    global_path = "./"
+def get_GLOBAL_NOTES_PATH():
+    try:
+        if os.path.exists(sys.argv[1]):
+            GLOBAL_NOTES_PATH = sys.argv[1]
+    except:
+        GLOBAL_NOTES_PATH = "./"
+
+    return GLOBAL_NOTES_PATH
+
+def get_assets_path():
+    a = os.path.realpath(__file__)
+    split = a.split("/")
+    split.pop()
+    joined = "/".join(split)
+    return joined 
+
+ASSETS_PATH = get_assets_path()
+GLOBAL_NOTES_PATH = get_GLOBAL_NOTES_PATH()
 
 def get_md_files():
-    p = os.listdir(global_path)
+    p = os.listdir(GLOBAL_NOTES_PATH)
     l = ["home"]
     for i in p:
         if "md" in i:
@@ -46,31 +54,34 @@ class PageContents:
         self.title = page.capitalize()
         self.contents = contents
 
+def create_routes():
+    @route('/')
+    @route('/home')
+    def home():
+        pretty_md_files = get_pretty_md_files()
+        check_home_address()
+        return template('{}/views/temp.tpl'.format(ASSETS_PATH), info="", md=pretty_md_files)
 
-@route('/')
-@route('/home')
-def home():
-    pretty_md_files = get_pretty_md_files()
-    check_home_address()
-    return template('{}views/temp.tpl'.format(global_path), info="", md=pretty_md_files)
+    @route('/<note>')
+    def r(note):
+        pretty_md_files = get_pretty_md_files()
+        if request.remote_addr != '127.0.0.1':
+            sys.exit()
+        f = '{}{}.md'.format(GLOBAL_NOTES_PATH, note) 
+        print(f)
+        with open(f, 'r') as f1:
+            buf = f1.read()
+        m = mistune.markdown(buf)
+        return template('{}/views/temp.tpl'.format(ASSETS_PATH), info=m, md=pretty_md_files)
 
-@route('/<note>')
-def r(note):
-    pretty_md_files = get_pretty_md_files()
-    if request.remote_addr != '127.0.0.1':
-        sys.exit()
-    f = '{}{}.md'.format(global_path, note) 
-    print(f)
-    with open(f, 'r') as f1:
-        buf = f1.read()
-    m = mistune.markdown(buf)
-    return template('{}views/temp.tpl'.format(global_path), info=m, md=pretty_md_files)
+    @route('/static/<filename>')
+    def server_static(filename):
+        return static_file(filename, root='{}/static'.format(ASSETS_PATH))
 
-@route('/static/<filename>')
-def server_static(filename):
-    return static_file(filename, root='{}static'.format(global_path))
+
 
 def start_server():
+    create_routes()
     run(host='localhost', port=3000, reloader=True)
 
 if __name__ == "__main__":
