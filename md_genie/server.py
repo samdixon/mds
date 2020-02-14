ce2a74417b7a898ef882b27d28cb045fdc12d7ba
samdixon <sdixon@posteo.net>
2020-02-14 10:13:09 -0500

    Server updates
    imports pathlib and utilizes globbing to grab sub markdown files
    adds get_markdown_files function to implement globbing of *.md files
    
    implements MarkdownGeniePath class
    each markdown file path will create an instance of this class.
    is is used as a helper class to resolve all links related to the md file
    class also implements several helper functions for resolving link issues
    This class also allows the removal of several functions dealing with md
    'prettying'
    
    removes ASSETS_PATH and GLOBAL_NOTES_PATH in favor of config class and
    the newly implemented MarkdownGeniePath class
    
    create routes function now has di.
    
    start server function now takes inputs from config instead of global or
    static vars

--- md_genie/server.py
+++ md_genie/server.py
@@ -1,86 +1,74 @@
 import os
+from pathlib import Path
 import sys
 from bottle import request, route, run, template, static_file
 import mistune
 
-def get_global_notes_path():
-    try:
-        if os.path.exists(sys.argv[1]):
-            GLOBAL_NOTES_PATH = sys.argv[1]
-    except:
-        GLOBAL_NOTES_PATH = "./"
+def get_markdown_files(notepath):
+    markdown_files = [] 
+    for path in Path(notepath).rglob("*.md"):
+        markdown_files.append(MarkdownGeniePath(notepath, path.as_posix(), path.name))
 
-    return GLOBAL_NOTES_PATH
+    return markdown_files
 
-def get_assets_path():
-    a = os.path.realpath(__file__)
-    split = a.split("/")
-    split.pop()
-    joined = "/".join(split)
-    return joined 
+class MarkdownGeniePath:
+    """
+    Simple data class to more easily work with absolute path names and filenames
+    """
+    def __init__(self, notepath, absolute_path, filename):
+        self.notepath = notepath
+        self.absolute_path = absolute_path
+        self.filename = filename
+        self.pretty_filename = self._get_pretty_name()
+        self.url_path = self._get_url_path()
 
-ASSETS_PATH = get_assets_path()
-GLOBAL_NOTES_PATH = get_global_notes_path()
+    def __str__(self):
+        return f"{self.absolute_path}: {self.filename}"
 
-def get_md_files():
-    p = os.listdir(GLOBAL_NOTES_PATH)
-    l = ["home"]
-    for i in p:
-        if "md" in i:
-            l.append(i)
-        else:
-            pass
-    return l
+    def _get_pretty_name(self):
+        ## Need to make sure this doesn't pop a file without an md tag
+        temp = self.filename.split(".")
+        return temp[0]
 
-def prettify_md_files(markdown_files):
-    pretty_md_files = []
-    for i in markdown_files:
-        temp = i.split(".")
-        pretty_md_files.append(temp[0])
+    def _get_url_path(self):
+        s = self.absolute_path.split("/")
+        url_path_arr = []
+        for i in s:
+            if i not in self.notepath:
+                url_path_arr.append(i)
 
-    return pretty_md_files
+        url_path_arr.pop()
+        url_path_arr.append(self.pretty_filename)
+        return "/".join(url_path_arr)
 
-def get_pretty_md_files():
-    return prettify_md_files(get_md_files())
-    
-def check_home_address():
-    if request.remote_addr != '127.0.0.1':
-        sys.exit()
-
-# Simple data structure for passing context to pages
-class PageContents:
-    def __init__(self, page, contents):
-        self.page = page
-        self.title = page.capitalize()
-        self.contents = contents
-
-def create_routes():
+def create_routes(assets_path, notepath, markdown_files):
     # TODO find better place for this var
-    main_template = "{}/views/main.tpl".format(ASSETS_PATH)
+    main_template = "{}/views/main.tpl".format(assets_path)
 
     @route('/')
     @route('/home')
     @route('/index')
     def index():
-        check_home_address()
-        pretty_md_files = get_pretty_md_files()
-        return template(main_template, info="", md=pretty_md_files)
+        return template(main_template, info="", md=markdown_files)
 
     @route('/<note>')
     def r(note):
-        check_home_address()
-        pretty_md_files = get_pretty_md_files()
-        markdown_note_path = '{}{}.md'.format(GLOBAL_NOTES_PATH, note) 
-        print(markdown_note_path)
+        ## This is a fucking hack
+        ## Needs to be rewritten.
+        ## This should pull from some other source. Not usre now. Think about it
+        for markdown_file in markdown_files:
+            if markdown_file.pretty_filename == note:
+                markdown_note_path = f"{markdown_file.absolute_path}"
         with open(markdown_note_path, 'r') as markdown_note:
             buf = markdown_note.read()
         rendered_markdown_note = mistune.markdown(buf)
-        return template(main_template, info=rendered_markdown_note, md=pretty_md_files)
+        return template(main_template, info=rendered_markdown_note, md=markdown_files)
 
     @route('/static/<filename>')
     def server_static(filename):
-        return static_file(filename, root='{}/static'.format(ASSETS_PATH))
+        return static_file(filename, root='{}/static'.format(assets_path))
 
-def start_server():
-    create_routes()
-    run(host='localhost', port=3000, reloader=True)
+def start_server(c):
+    markdown_files = get_markdown_files(c.notepath)
+    create_routes(c.assets_path, c.notepath, markdown_files)
+    run(host=c.host, port=3000, reloader=True)
